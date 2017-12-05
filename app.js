4cc4e133a45a21d0013b95af0eacb90a0c46b0f4
Jake See <jakesee@gmail.com>
2017-12-05 12:35:41 +0800

    added release branch

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-// environment
-var config = {};
-config.apiKey = process.env.apiKey;
-config.apiSecret = process.env.apiSecret;
-config.xfersId = process.env.xfersId;
-config.ccId = process.env.ccId;
+var app = function() {
+	// environment
+	var config = {};
+	config.apiKey = process.env.apiKey;
+	config.apiSecret = process.env.apiSecret;
+	config.xfersId = process.env.xfersId;
+	config.ccId = process.env.ccId;
+}
+
+app.prototype.run = function(routine) {
+	// Create the game loop
+	var tick = require('animation-loops');
+	var lastTime = 0;
+	var max = 120;
+	var handle = tick.add(routine);
+};
+
+
 
 // utitlity
 var calculator = require('./calculator.js');
@@ -28,75 +40,7 @@ var exchange = require('./exchange.js');
 exchange = new exchange(client);
 wait.for.promise(exchange.initPortfolio());
 
-// Create the game loop
-var tick = require('animation-loops');
-var lastTime = 0;
-var max = 120;
-var handle = tick.add(function(elapsed, delta, stop) {
 
-	// Rate-limiting is 10,000 per hour or 
-	if(elapsed - lastTime >= 5000)
-	{
-		lastTime = elapsed;
-
-		var promises = [
-			exchange.getBuyCommit('BTC', config.xfersId, max),
-			// exchange.getBuyCommit('LTC', config.xfersId, 1000),
-			// exchange.getSpotPrice('BTC', 'SGD'),
-			// exchange.getSpotPrice('BTC', 'USD'),
-
-			// exchange.getBuyCommit('LTC', config.xfersId, 1000),
-			// exchange.getSpotPrice('LTC', 'SGD'),
-			// exchange.getSpotPrice('LTC', 'USD'),
-		];
-
-		Promise.all(promises).then(values => {		
-
-			max++;
-			var btcTx = values[0];
-			var btcQuote = getQuote(values[0]);
-			// var ltcTx = values[1];
-			// var ltcQuote = getQuote(values[1]);		
-
-			// var data = [{
-			// 	'Value': exchange.portfolio.LTC.principal,
-			// 	'Spot BTC/SGD': values[1],
-			// 	'Spot BTC/USD': values[2],
-			// 	'Quote BTC/SGD': quote,
-			// 	'Buy BTC/SGD': calc.targetBuyPrice,
-			// }];
-
-			// // echo data table
-			// console.log(columnify([{
-			// 		'Quote BTC/SGD': btcQuote,
-			// 		'Limit BTC/SGD': BTC.targetBuyPrice,
-
-			// 		// 'Quote LTC/SGD': ltcQuote,
-			// 		// 'Limit LTC/SGD': LTC.targetBuyPrice,
-			// 	}]));
-
-			if(checkBuy(btcQuote, BTC.targetBuyPrice, btcTx))
-			{
-				BTC.targetBuyPrice = 0;
-				stop();
-			}
-
-			// if(checkBuy(ltcQuote, LTC.targetBuyPrice, ltcTx))
-			// {
-			// 	LTC.targetBuyPrice = 0;
-			// }
-
-			// if(LTC.targetBuyPrice + BTC.targetBuyPrice == 0)
-			// {
-			// 	stop();
-			// }
-
-		}, failed => {
-			// console.log('Cannot buy ', max);
-			max--;
-		});
-	}
-});
 
 
 function getQuote(tx)
