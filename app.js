e8f4f128f14ed60a13e7a78aabab20769ca1bc1f
Jake See <jakesee@gmail.com>
2017-12-07 23:51:01 +0800

    update

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ traders.push(new Trader('LTC2', calculator, { type: 'buy', fund: 500, currency: 
 traders.push(new Trader('LTC3', calculator, { type: 'buy', fund: 500, currency: 'LTC', limit: 124.87, paymentMethodId: config.xfersId }));
 traders.push(new Trader('LTC4', calculator, { type: 'buy', fund: 500, currency: 'LTC', limit: 128.44, paymentMethodId: config.xfersId }));
 
+traders.push(new Trader('BTC1', calculator, { type: 'buy', fund: 500, currency: 'BTC', l1imit: 18839.32, paymentMethodId: config.xfersId }));
 traders.push(new Trader('BTC1', calculator, { type: 'buy', fund: 500, currency: 'BTC', limit: 18839.32, paymentMethodId: config.xfersId }));
 traders.push(new Trader('BTC2', calculator, { type: 'buy', fund: 500, currency: 'BTC', limit: 18480.48, paymentMethodId: config.xfersId }));
 traders.push(new Trader('BTC3', calculator, { type: 'buy', fund: 500, currency: 'BTC', limit: 17967.13, paymentMethodId: config.xfersId }));
@@ -53,4 +54,4 @@ exchange.events.on(ec.eventNames.spotprice, (exchange, spot) => {
 // start the trading!
 var portfolio = wait.for.promise(exchange.init());
 console.log(columnify(exchange.getBillboard()));
-exchange.run(5000).live();
+exchange.run(5000);
