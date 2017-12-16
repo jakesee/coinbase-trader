a13895d85bc57f9bb1ace14d1c792ebb3c62e08e
Jake See <jakesee@gmail.com>
2017-12-16 19:58:50 +0800

    added test strategy

--- gdax.js
+++ gdax.js
@@ -10,7 +10,7 @@ module.exports = function(products) {
     var products = products;
     _.each(products, product => {
     	feed[product] = {
-    		'ticker': { time: null, price:0, last_size: 0, sequence: 0 },
+    		'ticker': { price:0, last_size: 0, sequence: 0, volume: 0 },
     		'bids': {}, // hash
     		'asks': {}, // hash
     	}
@@ -61,10 +61,31 @@ module.exports = function(products) {
             } else if(data.type == 'ticker') {
                 if(data.sequence > feed[product_id].ticker.sequence)
                 {
+       			    // { type: 'ticker',
+					  // sequence: 1707529262,
+					  // product_id: 'ETH-USD',
+					  // price: 651.13,
+					  // open_24h: '736.79000000',
+					  // volume_24h: '388070.47936022',
+					  // low_24h: '651.13000000',
+					  // high_24h: '736.82000000',
+					  // volume_30d: '8730314.94006487',
+					  // best_bid: '651.13',
+					  // best_ask: '651.14',
+					  // side: 'sell',
+					  // time: '2017-12-15T12:44:24.164000Z',
+					  // trade_id: 21429648,
+					  // last_size: 0.00230368 }
                     data.price = Number(data.price);
-                    data.last_size = Number(data.last_size);
+                    data.last_size = Number(data.last_size || 0);
+                    data.volume = feed[product_id].ticker.volume;
+					/*
+                	selling = asking, buying = bidding
+                	if side = buy, then price is ask price, because buyer yield to seller, therefore spot price increase
+                	if side = sell, then price is buy price, because seller yield to buyer, therefore spot price decrease
+                	*/
+                    data.volume += data.last_size * (data.side === 'buy' ? 1 : -1);
                 	feed[product_id].ticker = data;
-                    console.log(data);
                 }
             }
         });
--- test.js
+++ test.js
@@ -4,9 +4,13 @@ var columnify = require('columnify');
 var _ = require('lodash');
 // formatting numbers: http://mathjs.org/docs/reference/functions/format.html
 
-var gdax = new Gdax(['BTC-USD', 'ETH-USD', 'ETH-BTC', 'LTC-USD', 'LTC-BTC']);
+const products = ['BTC-USD', 'ETH-USD', 'ETH-BTC', 'LTC-USD', 'LTC-BTC'];
+var gdax = new Gdax(products);
 
 gdax.run();
+
+var wallet = { 'LTC': 0, 'BTC': 0.16880029726516052 };
+var action = 'buy';
 
 var lastTime = 0;
 tick.add((elapsed, delta, stop) => {
@@ -35,66 +39,64 @@ tick.add((elapsed, delta, stop) => {
 	//   trade_id: 28187314,
 	//   last_size: '0.01900000' }
 
-	var output = [
-		{ 
-			'Product': 'BTC-USD',
-			'Price': Number(data['BTC-USD'].ticker.price),
-			'Side': data['BTC-USD'].ticker.side,
-			'low_24h': data['BTC-USD'].ticker.low_24h,
-			'high_24h': data['BTC-USD'].ticker.high_24h,
-			'best_bid': data['BTC-USD'].ticker.best_bid,
-			'best_ask': data['BTC-USD'].ticker.best_ask,
-			'bid_size': data['BTC-USD'].bids[0].size,
-			'ask_size': data['BTC-USD'].asks[0].size
-		},
-		{ 
-			'Product': 'LTC-USD',
-			'Price': Number(data['LTC-USD'].ticker.price),
-			'Side': data['LTC-USD'].ticker.side,
-			'low_24h': data['LTC-USD'].ticker.low_24h,
-			'high_24h': data['LTC-USD'].ticker.high_24h,
-			'best_bid': data['LTC-USD'].ticker.best_bid,
-			'best_ask': data['LTC-USD'].ticker.best_ask,
-			'bid_size': data['LTC-USD'].bids[0].size,
-			'ask_size': data['LTC-USD'].asks[0].size
-		},
-		{ 
-			'Product': 'LTC-BTC',
-			'Price': Number(data['LTC-BTC'].ticker.price),
-			'Side': data['LTC-BTC'].ticker.side,
-			'low_24h': data['LTC-BTC'].ticker.low_24h,
-			'high_24h': data['LTC-BTC'].ticker.high_24h,
-			'best_bid': data['LTC-BTC'].ticker.best_bid,
-			'best_ask': data['LTC-BTC'].ticker.best_ask,
-			// 'bid_size': data['LTC-BTC'].bids[0].size,
-			// 'ask_size': data['LTC-BTC'].asks[0].size
-		},
-		{ 
-			'Product': 'LTC-BTC*',
-			'Price': (Number(data['LTC-USD'].ticker.price) / Number(data['BTC-USD'].ticker.price)),
-			'bid_size': _.reduce(_.take(data['LTC-BTC'].bids, 3), (total, offer) => { return total + offer.price * offer.size }, 0),
-			'ask_size': _.reduce(_.take(data['LTC-BTC'].asks, 3), (total, offer) => { return total + offer.price * offer.size }, 0)
-		},
-		{ 
-			'Product': 'ETH-BTC',
-			'Price': Number(data['ETH-BTC'].ticker.price),
-			'Side': data['ETH-BTC'].ticker.side,
-			'low_24h': data['ETH-BTC'].ticker.low_24h,
-			'high_24h': data['ETH-BTC'].ticker.high_24h,
-			'best_bid': data['ETH-BTC'].ticker.best_bid,
-			'best_ask': data['ETH-BTC'].ticker.best_ask,
-			// 'bid_size': data['ETH-BTC'].bids[0].size,
-			// 'ask_size': data['ETH-BTC'].asks[0].size
-		},
-		{ 
-			'Product': 'ETH-BTC*',
-			'Price': (Number(data['ETH-USD'].ticker.price) / Number(data['BTC-USD'].ticker.price)),
-			'bid_size': _.reduce(_.take(data['ETH-BTC'].bids, 3), (total, offer) => { return total + offer.price * offer.size }, 0),
-			'ask_size': _.reduce(_.take(data['ETH-BTC'].asks, 3), (total, offer) => { return total + offer.price * offer.size }, 0)
-		},
-	];
+	var output = [];
+	_.each(products, product => {
 
-	// console.log(columnify(output));
+		let bid_size = _.reduce(_.take(data[product].bids, 3), (total, offer) => { return total + offer.price * offer.size }, 0);
+		let ask_size = _.reduce(_.take(data[product].asks, 3), (total, offer) => { return total + offer.price * offer.size }, 0);
+
+		output.push({
+			'Product': product,
+			'Price': data[product].ticker.price,
+			'Volume': data[product].ticker.volume,
+			'Side': data[product].ticker.side,
+			'low_24h': data[product].ticker.low_24h,
+			'high_24h': data[product].ticker.high_24h,
+			'best_bid': data[product].ticker.best_bid,
+			'best_ask': data[product].ticker.best_ask,
+			'bid_size': bid_size,
+			'ask_size': ask_size,
+			'bid-ask': bid_size / ask_size,
+		});
+	});
+
+	output.push({ 
+		'Product': 'LTC-BTC*',
+		'Price': (Number(data['LTC-USD'].ticker.price) / Number(data['BTC-USD'].ticker.price)),
+		// 'bid_size': _.reduce(_.take(data['LTC-BTC'].bids, 3), (total, offer) => { return total + offer.price * offer.size }, 0),
+		// 'ask_size': _.reduce(_.take(data['LTC-BTC'].asks, 3), (total, offer) => { return total + offer.price * offer.size }, 0),
+	});
+	// output.push({ 
+	// 	'Product': 'ETH-BTC*',
+	// 	'Price': (Number(data['ETH-USD'].ticker.price) / Number(data['BTC-USD'].ticker.price)),
+	// 	'bid_size': _.reduce(_.take(data['ETH-BTC'].bids, 3), (total, offer) => { return total + offer.price * offer.size }, 0),
+	// 	'ask_size': _.reduce(_.take(data['ETH-BTC'].asks, 3), (total, offer) => { return total + offer.price * offer.size }, 0)
+	// });
+
+	var equiPrice = (Number(data['LTC-USD'].ticker.price) / Number(data['BTC-USD'].ticker.price));
+	if((action == 'sell' || action == null) && data['LTC-BTC'].ticker.price > equiPrice && wallet.LTC > 0)
+	{
+		wallet.BTC = wallet.LTC * data['LTC-BTC'].ticker.price;
+		console.log("SELL", wallet.LTC, "LTC @", data['LTC-BTC'].ticker.price, '=>', wallet.BTC);
+		wallet.LTC = 0;
+		action = 'buy';
+		
+		console.log(columnify([wallet]));
+	}
+	else if((action == 'buy' || action == null)  && data['LTC-BTC'].ticker.price < equiPrice && wallet.BTC > 0)
+	{
+		wallet.LTC = wallet.BTC / data['LTC-BTC'].ticker.price;
+		console.log("BUY", wallet.LTC, "LTC @", data['LTC-BTC'].ticker.price, '<=', wallet.BTC);
+		wallet.BTC = 0;
+		action = 'sell';
+		console.log(columnify([wallet]));
+	}
+	else
+	{
+		// console.log(_.take(data['LTC-BTC'].bids, 3));
+		// console.log(columnify(output));
+		// console.log(columnify([wallet]));
+	}
 });
 
 
@@ -106,4 +108,9 @@ BUY LTC with
 if LTC-BTC is low because BTC is high and LTC is low, then buy LTC
 if LTC-BTC is low because both BTC and LTC is low, then hold.
 ll
+
+When the bid volume is higher than the ask volume, the selling is stronger, and the price is likely to move lower.
+When the ask volume is higher than the bid volume, the buying is stronger, and the price is likely to move higher.
 */
+
+
